edf90ca1cda5c067c2328c5176f4e7a8e6a27044
Gustavo-N5 <gustavonobrega37@gmail.com>
2023-12-03 23:36:22 -0300

    voltando a tela de italo

--- src/components/TelaInicial.vue
+++ src/components/TelaInicial.vue
@@ -1,56 +1,55 @@
 <template>
-<body>
+  <body>
     <div class="container-fluid">
-    <div id="geral" class="row">
+      <div id="geral" class="row">
         <nav id="sidebar" class="col-md-2 col-sm-12">
-            <img id="img_nav" src="../assets/logo_biolab.png">
-            <div id="botoes_laterais">
-                <div class="item_lateral">
-                    <button class="btn btn-outline-success btn-lg" @click="exibirlist()">Atendimento</button>
-                    <div class="lista_opc" v-show="listI">
-                        <button class="btn btn-outline-secondary border-0" @click="exibirAtendimento()">Novo Atendimento</button>
-                        <button class="btn btn-outline-secondary border-0" @click="exibirNovoPaciente()">Novo Paciente</button>
-                        <button class="btn btn-outline-secondary border-0" @click="exibirInserirLaudo()">Inserir Laudo</button>
-                    </div>
-                </div>
-                </div>
-                <div class="item_lateral">
-                    <button class="btn btn-outline-success btn-lg" @click="exibirlist2()">Configuração</button>
-                    <div class="lista_opc" v-show="listII">
-                        <button class="btn btn-outline-secondary border-0" @click="exibirCadColab()">Cadastro Colaborador</button>
-                        <button class="btn btn-outline-secondary border-0" @click="exibirCadProce()">Cadastro Procedimento</button>
-                    </div>
-                </div>
+          <img id="img_nav" src="../assets/logo_biolab.png">
+          <div id="botoes_laterais">
+            <div class="item_lateral">
+              <button class="btn btn-outline-success btn-lg" @click="exibirlist()">Atendimento</button>
+              <div class="lista_opc" v-show="listI">
+                <button class="btn btn-outline-secondary border-0" @click="exibirAtendimento()">Novo Atendimento</button>
+                <button class="btn btn-outline-secondary border-0" @click="exibirNovoPaciente()">Novo Paciente</button>
+                <button class="btn btn-outline-secondary border-0" @click="exibirInserirLaudo()">Inserir Laudo</button>
+              </div>
+            </div>
+          </div>
+          <div class="item_lateral">
+            <button class="btn btn-outline-success btn-lg" @click="exibirlist2()">Configuração</button>
+            <div class="lista_opc" v-show="listII">
+              <button class="btn btn-outline-secondary border-0" @click="exibirCadColab()">Cadastro Colaborador</button>
+              <button class="btn btn-outline-secondary border-0" @click="exibirCadProce()">Cadastro Procedimento</button>
+            </div>
+          </div>
 
         </nav>
-        <div class="col-md-10 col-sm-12"> 
-            <div class="telas_forms" v-show="dashboard"> 
-                <Dashboard/>
-            </div>
-            <div class="telas_forms" v-show="atendimento"> 
-                <Atendimento/>
-            </div>
-            <div class="telas_forms" v-show="novo_paciente">
-                <NovoPaciente/>
-            </div>
-            <div class="telas_forms" v-show="inserir_laudo"> 
-                <InserirLaudo/>
-            </div>
-            <div class="telas_forms" v-show="cad_colab"> 
-                <CadastroColab/>
-            </div>
-            <div class="telas_forms" v-show="cad_proc">
-                <CadastroProce/>
-            </div>
-            </div>
+        <div class="col-md-10 col-sm-12">
+          <div class="telas_forms" v-show="dashboard">
+            <Dashboard />
+          </div>
+          <div class="telas_forms" v-show="atendimento">
+            <Atendimento />
+          </div>
+          <div class="telas_forms" v-show="novo_paciente">
+            <NovoPaciente />
+          </div>
+          <div class="telas_forms" v-show="inserir_laudo">
+            <InserirLaudo />
+          </div>
+          <div class="telas_forms" v-show="cad_colab">
+            <CadastroColab />
+          </div>
+          <div class="telas_forms" v-show="cad_proc">
+            <CadastroProce />
+          </div>
         </div>
-        <div class="col-sm-12">
-            <button @click="sair()" id="sair" class="btn btn-danger">Sair</button>
-        </div> 
+      </div>
+      <div class="col-sm-12">
+              <button @click="sair()" id="sair" class="btn btn-danger">Sair</button>
+            </div>
+    </div>
 
-        </div>
-
-</body>
+  </body>
 </template>
 
 <script>
@@ -61,170 +60,173 @@ import CadastroColab from '../forms/CadastroColab.vue';
 import CadastroProce from '../forms/CadastroProce.vue';
 import Dashboard from './Dashboard.vue';
 
-export default{
-    name:"TelaInicial",
-    components:{
-        Atendimento,
-        NovoPaciente,
-        InserirLaudo,
-        CadastroColab,
-        CadastroProce,
-        Dashboard
-    },
-    data(){
-        return{
-            listI : false,
-            listII: false,
-            atendimento: false,
-            novo_paciente: false,
-            pesquisar_pac: false,
-            inserir_laudo: false,
-            exibir_relatorio: false,
-            cad_colab: false,
-            cad_proc: false,
-            dashboard: true,
-
-        }
-    },
-    methods:{
-        exibirlist(){
-            this.listI = !this.listI;
-            this.listII = false;
-        },
-        exibirlist2(){
-            this.listII = !this.listII;
-            this.listI = false;
-        },
-        exibirAtendimento(){
-           this.atendimento = !this.atendimento;
-           this.novo_paciente = false;
-           this.pesquisar_pac = false;
-           this.inserir_laudo = false;
-           this.cad_colab = false;
-           this.cad_proc = false;
-           this.dashboard = !this.dashboard
-
-        },
-        exibirNovoPaciente(){
-            this.novo_paciente = !this.novo_paciente;
-            this.atendimento = false;
-            this.pesquisar_pac = false;
-            this.inserir_laudo = false;
-            this.exibir_relatorio = false;
-            this.cad_colab = false;
-            this.cad_proc = false;
-            this.dashboard = !this.dashboard
-        },
-        exibirInserirLaudo(){
-            this.inserir_laudo = !this.inserir_laudo;
-            this.atendimento = false;
-            this.novo_paciente = false;
-            this.pesquisar_pac = false;
-            this.exibir_relatorio = false;
-            this.cad_colab = false
-            this.cad_proc = false;
-        },
-        exibirCadColab(){
-            this.cad_colab = !this.cad_colab
-            this.cad_proc = false;
-            this.atendimento = false;
-            this.novo_paciente = false;
-            this.inserir_laudo = false;
-            this.dashboard = !this.dashboard
-        },
-        exibirCadProce(){
-            this.cad_proc = !this.cad_proc;
-            this.cad_colab = false;
-            this.atendimento = false;
-            this.novo_paciente = false;
-            this.inserir_laudo = false;
-            this.dashboard = !this.dashboard
-          
-        },
-        sair(){
-            this.$router.push('/')
-        }
-
+export default {
+  name: "TelaInicial",
+  components: {
+    Atendimento,
+    NovoPaciente,
+    InserirLaudo,
+    CadastroColab,
+    CadastroProce,
+    Dashboard
+  },
+  data() {
+    return {
+      listI: false,
+      listII: false,
+      atendimento: false,
+      novo_paciente: false,
+      pesquisar_pac: false,
+      inserir_laudo: false,
+      exibir_relatorio: false,
+      cad_colab: false,
+      cad_proc: false,
+      dashboard: true,
 
     }
+  },
+  methods: {
+    exibirlist() {
+      this.listI = !this.listI;
+      this.listII = false;
+    },
+    exibirlist2() {
+      this.listII = !this.listII;
+      this.listI = false;
+    },
+    exibirAtendimento() {
+      this.atendimento = !this.atendimento;
+      this.novo_paciente = false;
+      this.pesquisar_pac = false;
+      this.inserir_laudo = false;
+      this.cad_colab = false;
+      this.cad_proc = false;
+      this.dashboard = !this.dashboard
+
+    },
+    exibirNovoPaciente() {
+      this.novo_paciente = !this.novo_paciente;
+      this.atendimento = false;
+      this.pesquisar_pac = false;
+      this.inserir_laudo = false;
+      this.exibir_relatorio = false;
+      this.cad_colab = false;
+      this.cad_proc = false;
+      this.dashboard = !this.dashboard
+    },
+    exibirInserirLaudo() {
+      this.inserir_laudo = !this.inserir_laudo;
+      this.atendimento = false;
+      this.novo_paciente = false;
+      this.pesquisar_pac = false;
+      this.exibir_relatorio = false;
+      this.cad_colab = false
+      this.cad_proc = false;
+    },
+    exibirCadColab() {
+      this.cad_colab = !this.cad_colab
+      this.cad_proc = false;
+      this.atendimento = false;
+      this.novo_paciente = false;
+      this.inserir_laudo = false;
+      this.dashboard = !this.dashboard
+    },
+    exibirCadProce() {
+      this.cad_proc = !this.cad_proc;
+      this.cad_colab = false;
+      this.atendimento = false;
+      this.novo_paciente = false;
+      this.inserir_laudo = false;
+      this.dashboard = !this.dashboard
+
+    },
+    sair() {
+      this.$router.push('/')
+    }
+
+
+  }
 }
 </script>
 
 <style scoped>
-
-.item_lateral .btn btn-outline-success{
-    width: 100%;
-
-}
-.btn btn-outline-secondary{
-    border: none;
-}
-
-#sidebar{
-    background-color:#B4C2B4;
-    bottom: 0;
-    top:0;
-    left:0;
-    overflow-y:auto;
-}
-#botoes_laterais{
-    display: block;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-}
-.item_lateral{
-    background-color: #fff;
-    display: grid;
-    width: 100%;
-    orientation: colunm;
-    border-radius: 10px;
-    align-items: center;
-}
-
-#sidebar #img_nav{
-    width: 100%;
-}
-
-.container-fluid{
-    background-color:#B4C2B4;
-    overflow-y:auto;
-    position: fixed;
-    right:0;
-    bottom: 0;
-    top: 0;
-}
-
-.lista_opc{
-    background-color: #fff;
-    border-radius: 10px;
-    display: grid;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    padding-top:2px;
-    padding-bottom:5px;
-}
-.telas_forms{
-    background-color: #B4C2B4;
+.item_lateral .btn btn-outline-success {
+  width: 100%;
 
 }
-.col-md-10{
-    background-color: #B4C2B4;
-    overflow-y:auto;
-    margin-top:30px;
-    left:0;
-    right:0;
-    bottom: 0;
-    top: 0;
-    align-items: center;
-    border-right:#70B446;
-}
-#sair{
-    bottom: 90%;
-    left: 95%;
-    position: fixed;
+
+.btn btn-outline-secondary {
+  border: none;
 }
 
+#sidebar {
+  background-color: #B4C2B4;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  overflow-y: auto;
+}
 
+#botoes_laterais {
+  display: block;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.item_lateral {
+  background-color: #fff;
+  display: grid;
+  width: 100%;
+  orientation: colunm;
+  border-radius: 10px;
+  align-items: center;
+}
+
+#sidebar #img_nav {
+  width: 100%;
+}
+
+.container-fluid {
+  background-color: #B4C2B4;
+  overflow-y: auto;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  top: 0;
+}
+
+.lista_opc {
+  background-color: #fff;
+  border-radius: 10px;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  padding-top: 2px;
+  padding-bottom: 5px;
+}
+
+.telas_forms {
+  background-color: #B4C2B4;
+
+}
+
+.col-md-10 {
+  background-color: #B4C2B4;
+  overflow-y: auto;
+  margin-top: 30px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  align-items: center;
+  border-right: #70B446;
+}
+
+#sair {
+  bottom: 93.5%;
+  left: 94.5%;
+  position: fixed;
+}
 </style>
--- src/forms/CadastroColab.vue
+++ src/forms/CadastroColab.vue
@@ -1,95 +1,102 @@
 <template>
-  <div id="formulario_paciente" class="d-flex justify-content-center">
-    <form class="formulario" @submit.prevent="salvarColab()" ref="form">
-      <h2>Cadastro Colaborador</h2>
-      <div style=" display: flex; justify-content: center;">
-      </div>
-      <div class="row">
-        <div class="col-md-2 col-12">
-          <label>Cpf</label>
-          <input type="text" class="form-control " v-model="cpf" placeholder="999.999.999.99"
-            v-mask="['###.###.###-##', '###.###.###-##']">
-
-        </div>
-        <div class="col-md-10 col-12">
-          <label>Nome Completo</label>
-          <input type="text" class="form-control" v-model="nome" placeholder="Nome Completo">
-
-        </div>
-        <div class="col-md-10 col-12">
-          <label>Email</label>
-          <input type="text" class="form-control" v-model="email" placeholder="meuemail@gmail.com">
-
-        </div>
-        <div class="col-md-2">
-          <label>Data de nascimento</label>
-          <input type="date" class="form-control" v-model="dataNascimento">
-
-        </div>
-        <div class="col-md-4">
-          <label>Sexo</label>
-          <select class="form-select" v-model="sexo">
-            <option :value="''" selected></option>
-            <option :value="'M'">Masculino</option>
-            <option :value="'F'">Feminino</option>
-          </select>
-
-        </div>
-        <div class="col-md-8">
-          <label>Telefone</label>
-          <input type="text" class="form-control" placeholder="(83)99999-9999" v-model="telefone">
-
-        </div>
-        <div class="col-md-6">
-          <label>Senha de Acesso</label>
-          <input type="password" class="form-control" v-model="senha" placeholder="******">
-
-        </div>
-        <h3 class="mt-3">Endereço</h3>
-        <div class="col-md-2">
-          <span></span>
-          <label for="cep">Cep</label>
-          <input type="text" id="cep" v-model="endereco.cep" @input="searchCep" class="form-control" placeholder="0000-000"
-            v-mask="['#####-###', '#####-###']">
-
-        </div>
-        <div class="col-md-10">
-          <span></span>
-          <label>Rua</label>
-          <input type="text" class="form-control" v-model="endereco.rua" readonly>
-
-        </div>
-        <div class="col-md-5">
-          <label>Bairro</label>
-          <input type="text" class="form-control" placeholder="" v-model="endereco.bairro">
-
-        </div>
-        <div class="col-md-5">
-          <label>Cidade</label>
-          <input type="text" class="form-control" placeholder="" v-model="endereco.cidade">
-
-        </div>
-        <div class="col-md-1">
-          <span></span>
-          <label>Estado</label>
-          <input type="text" class="form-control" placeholder="" v-model="endereco.estado">
-
-        </div>
-        <div class="col-md-1">
-          <label>Numero</label>
-          <input type="text" class="form-control" v-model="endereco.numero">
-        </div>
-        <div id="salvar" class="d-flex justify-content-end mt-5">
-          <input type="submit" class="btn btn-success large">
-        </div>
-      </div>
-    </form>
-  </div>
+    <div>
+    <h1>Cadastro Colaborador</h1>
+    <div id="formulario_paciente" class="p-5 container">
+        <form class="formulario" @submit.prevent="salvarColab()">
+          <div style=" display: flex; justify-content: center;">
+          </div>
+            <div class="row">
+                <div class="col-md-4 col-12">
+                    <label>Cpf</label>
+                    <input type="text" class="form-control " v-model="cpf" placeholder="999.999.999.99" v-mask="['###.###.###-##', '###.###.###-##']" >
+                    <span v-if="erro.cpf" class="erro text-danger text-break">{{ erro.cpf }}</span>
+                </div>
+                <div class="col-md-8 col-12">
+                    <label>Nome Completo</label>
+                    <input type="text" class="form-control" v-model="nome" placeholder="Nome Completo" >
+                    <span v-if="erro.nome" class="erro text-danger text-break">{{ erro.nome }}</span>
+                </div>
+                <div class="col-md-6 col-12">
+                    <label>Email</label>
+                    <input type="text" class="form-control" v-model="email" placeholder="meuemail@gmail.com">
+                    <span v-if="erro.email" class="erro text-danger text-break">{{ erro.email }}</span>
+                </div>
+                <div class="col-md-6">
+                    <label>Data de nascimento</label>
+                    <input type="date" class="form-control" v-model="dataNascimento" >
+                    <span v-if="erro.dataNascimento" class="erro text-danger text-break">{{ erro.dataNascimento }}</span>
+                </div>
+                <div class="col-md-6">
+                    <span></span>
+                    <label for="cep">Cep</label>
+                    <input type="text" id="cep" v-model="cep" @input="searchCep" class="form-control" placeholder="0000000" v-mask="['#####-###', '#####-###']" >
+                    <span v-if="erro.endereco.cep" class="erro text-danger text-break">{{ erro.endereco.cep }}</span>
+                </div>
+                <div class="col-md-6">
+                    <label>Sexo</label>
+                    <select class="form-select" v-model="sexo">
+                        <option :value="''" selected></option>
+                        <option :value="'M'">Masculino</option>
+                        <option :value="'F'">Feminino</option>
+                    </select>
+                    <span v-if="erro.sexo" class="erro text-danger text-nowrap">{{ erro.sexo }}</span>
+                </div>
+                <div class="col-md-11">
+                    <span></span>
+                    <label>Rua</label>
+                    <input type="text" class="form-control" v-model="endereco.rua" readonly>
+                    <span v-if="erro.endereco.rua" class="erro text-danger text-nowrap">{{ erro.endereco.rua }}</span>
+                </div>
+                <div class="col-md-1">
+                    <span></span>
+                    <label>Numero</label>
+                    <input type="text" class="form-control" v-model="endereco.numero">
+                    <span v-if="erro.endereco.numero" class="erro text-danger text-nowrap">{{ erro.endereco.numero }}</span>
+                </div>
+                  <div class="row" style="justify-content: flex-start;">
+                    <div class="col-md-6">
+                        <span></span>
+                        <label>Bairro</label>
+                        <input type="text" class="form-control" placeholder=""  v-model="endereco.bairro" >
+                        <span v-if="erro.endereco.bairro" class="erro text-danger text-break">{{ erro.endereco.bairro }}</span>
+                    </div>
+                    <div class="row col-md-6">
+                        <div class="col-md-10">
+                            <span></span>
+                            <label>Cidade</label>
+                            <input type="text" class="form-control" placeholder="" v-model="endereco.cidade" >
+                            <span v-if="erro.endereco.cidade" class="erro text-danger text-nowrap">{{ erro.endereco.cidade}}</span>
+                        </div>
+                        <div class="col-md-2">
+                            <span></span>
+                            <label>Estado</label>
+                            <input type="text" class="form-control" placeholder="" v-model="endereco.estado" >
+                            <span v-if="erro.endereco.estado" class="erro text-danger text-nowrap">{{ erro.endereco.estado }}</span>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <label>Telefone</label>
+                    <input type="text" class="form-control" placeholder="(83)99999-9999" v-model="telefone" v-mask="['(##) ####-####', '(##) #####-####']">
+                    <span v-if="erro.telefone" class="erro text-danger text-nowrap">{{ erro.telefone }} </span>
+                </div>
+                <div class="col-md-6">
+                    <label>Senha de Acesso</label>
+                    <input type="password" class="form-control"  v-model="senha" placeholder="******" >
+                    <span v-if="erro.senha" class="erro text-danger text-nowrap">{{ erro.senha }}</span>
+                </div>
+                <div id="salvar">
+                    <input type="submit" class="btn btn-success large" >
+                </div>
+            </div>
+        </form>
+</div>
+</div>
 </template>
 
 <script>
 import axios from "axios";
-import request from '../router/Requests'
+import { required } from 'vuelidate/lib/validators'
 
 export default {
   name: "CadastroColab",
@@ -99,19 +106,36 @@ export default {
       cpf: "",
       dataNascimento: "",
       sexo: "",
-      telefone: "",
-      endereco: {
-        cep: "",
+      telefone:"",
+      endereco:{
         rua: "",
-        bairro: "",
-        numero: "",
-        cidade: "",
-        estado: ""
+        bairro:"",
+        numero:"",
+        cidade:"",
+        estado:"",
+
       },
       email: "",
       senha: "",
       tipoUsuario: "PADRAO",
       msg: "",
+      erro: {
+        nome: "",
+        cpf: "",
+        dataNascimento: "",
+        sexo: "",
+        email: "",
+        senha: "",
+        telefone:"",
+        endereco:{
+        rua: "",
+        bairro:"",
+        numero:"",
+        cidade:"",
+        estado:""
+      },
+      },
+      cep: '',
       cepData: null,
     };
   },
@@ -123,31 +147,94 @@ export default {
       formdata.append("dataNascimento", this.dataNascimento);
       formdata.append("sexo", this.sexo);
       formdata.append("telefone", this.telefone);
-      formdata.append("endereco.cep", this.endereco.cep);
       formdata.append("endereco.rua", this.endereco.rua);
       formdata.append("endereco.bairro", this.endereco.bairro);
       formdata.append("endereco.numero", this.endereco.numero);
       formdata.append("endereco.cidade", this.endereco.cidade);
       formdata.append("endereco.estado", this.endereco.estado);
+      formdata.append("endereco.cep", this.cepData.cep);
       formdata.append("email", this.email);
       formdata.append("senha", this.senha);
       formdata.append("tipoUsuario", this.tipoUsuario = "PADRAO");
 
-      request
-        .post("http://localhost:8080/api/usuario", formdata)
+      axios
+        .post("http://localhost:8080/api/usuario", formdata,{
+          headers: {
+        'Content-Type': 'application/json'
+          }
+        })
         .then(({ data }) => {
-          this.$toasted.success("Colaborador Cadastrado com sucesso!",);
-          this.$refs.form.reset();
+          this.$toasted.success("Colaborador cadastrado com sucesso!",);
+          this.msg = "Colaborador Cadastrado com Sucesso!";
+          this.limparFormulario()
         })
         .catch((error) => {
-          this.$toasted.error("Erro ao cadastrar Colaborador",);
+         this.$toasted.error("Erro ao cadastrar colaborador");
+         const mensagensErro = error.response.data;
+         console.log(error.response.data)
+         mensagensErro.forEach((erro) => {
+            if (erro.campo == "cpf"){
+                console.log(erro.mensagem)
+               this.erro.cpf = erro.mensagem
+            }
+            if (erro.campo == "nome"){
+                console.log(erro.mensagem)
+               this.erro.nome = erro.mensagem
+            }
+            if (erro.campo == "dataNascimento"){
+                console.log(erro.mensagem)
+               this.erro.dataNascimento = erro.mensagem
+            }
+            if (erro.campo == "sexo"){
+                console.log(erro.mensagem)
+               this.erro.sexo = erro.mensagem
+            }
+            if (erro.campo == "email"){
+                console.log(erro.mensagem)
+               this.erro.email = erro.mensagem
+            }
+            if (erro.campo == "telefone"){
+                console.log(erro.mensagem)
+               this.erro.telefone = erro.mensagem
+            }
+
+            if (erro.campo == "senha"){
+                console.log(erro.mensagem)
+               this.erro.senha = erro.mensagem
+            }
+            if (erro.campo == "endereco.rua"){
+                console.log(erro.mensagem)
+               this.erro.endereco.rua = erro.mensagem
+            }
+            if (erro.campo == "endereco.bairro"){
+                console.log(erro.mensagem)
+               this.erro.endereco.bairro = erro.mensagem
+            }
+            if (erro.campo == "endereco.cidade"){
+                console.log(erro.mensagem)
+               this.erro.endereco.cidade = erro.mensagem
+            }
+            if (erro.campo == "endereco.estado"){
+                console.log(erro.mensagem)
+               this.erro.endereco.estado = erro.mensagem
+            }
+            if (erro.campo == "endereco.numero"){
+                console.log(erro.mensagem)
+               this.erro.endereco.numero = erro.mensagem
+            }
+            if (erro.campo == "endereco.cep"){
+                console.log(erro.mensagem)
+               this.erro.endereco.cep = erro.mensagem
+            }
+
+            });
 
         });
 
     },
     async searchCep() {
       try {
-        const response = await axios.get(`https://viacep.com.br/ws/${this.endereco.cep}/json/`);
+        const response = await axios.get(`https://viacep.com.br/ws/${this.cep}/json/`);
         this.cepData = response.data;
         this.endereco.rua = this.cepData.logradouro
         this.endereco.bairro = this.cepData.bairro
@@ -157,27 +244,50 @@ export default {
         console.error('Erro ao buscar CEP:', error);
       }
     },
-  }
+    limparFormulario() {
+      // Resetar os valores para os estados iniciais
+      this.nome = "";
+      this.cpf = "";
+      this.dataNascimento = "";
+      this.sexo = "";
+      this.email = "";
+      this.senha = "";
+      this.telefone = "";
+      this.cep = "";
+      this.endereco.rua = "";
+      this.endereco.bairro = "";
+      this.endereco.cidade = "";
+      this.endereco.estado = "";
+      this.endereco.numero = "";
+
+
+    },
+
+
+  },
 };
 </script>
 
 <style scoped>
-#salvar {
-  align-items: baseline;
+
+#salvar{
+    align-items: baseline;
 }
 
-label {
-  margin-top: 10px;
-  font-weight: bold;
-  font-family: Arial, Helvetica, sans-serif;
+.col-md-6{
+    margin: 8px auto;
 }
 
-#formulario_paciente {
-  margin-top: 20px;
-  background-color: white;
-  border-radius: 18px;
-  padding: 10px;
-  height: 65%;
-  width: 100%;
+.formulario{
+    margin: 20px auto;
+    padding: 10px 10px;
 }
+
+#formulario_paciente{
+    background-color: #D9D9D9;
+    border-radius: 5px;
+    padding: 10px;
+    height: 80%;
+}
+
 </style>
